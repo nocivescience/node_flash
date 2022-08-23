c572196b4929fbe69571b8795bb48d0228637cd3
noceive <nocivescience@gmail.com>
2022-08-23 16:22:10 -0400

    terminar

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,17 +4,23 @@ const router = Router();
 router.get('/', (req, res) => {
     res.render('index');
 });
-
+router.post('/prueba1',(req,res)=>{
+    const {correo,contrasena}=req.body;
+    req.session.usuario={correo,contrasena};
+    res.redirect('/profile');
+})
+router.get('/profile',(req,res)=>{
+    const superusuario=req.session.usuario;
+    res.render('profile',{superusuario});
+});
 router.post('/register', (req, res) => {
     const { email, password } = req.body;
     req.session.user_data = {email, password};
-    req.flash('success', 'Now You are Registered')
     res.redirect('/profile');
 });
 
 router.get('/profile', (req, res) => {
     const user = req.session.user_data;
-    console.log(user);
     delete req.session.user_data;
 
     res.render('profile', {
